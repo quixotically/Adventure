cf36aa9214c22d0bbd606c8a68305b77f0fa33d5
Felicia Wong <felicia.wong@schrodinger.com>
2018-01-23 14:05:05 -0500

    First models and view

--- game/models.py
+++ game/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Situation(models.Model):
+    title = models.CharField(max_length=30)
+    situation_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    situation = models.ForeignKey(Situation, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    next_situation = models.ForeignKey(Situation, on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return self.choice_text
